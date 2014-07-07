26097c129d0b02154f46fdbe2dc929540b9e2ff7
Richard Sharp <richpsharp@gmail.com>
2014-07-07 16:50:58 -0700

    summarize results
    It takes a while to go through the csv, make a separate app for that.

---
+++ summarize_csv_results.py
@@ -0,0 +1,50 @@
+import os
+import collections
+import time
+
+CSV_DIR = os.path.join("C:\\", "Users", "rich", "Desktop", "forest_edge_output")
+
+if __name__ == '__main__':
+    for prefix in ['am', 'af', 'as']:
+        biomass_stats_uri = os.path.join(
+            CSV_DIR, "%s_biomass_stats.csv" % prefix)
+        biomass_file = open(biomass_stats_uri, 'rU')
+
+        print 'calculating lines'
+        num_lines = sum(1 for line in open(biomass_stats_uri, 'rU'))
+        print 'number of lines: %d' % num_lines
+        distance_biomass_lookup = collections.defaultdict(list)
+        start_time = time.time()
+        count = 0
+        for line in biomass_file:
+            distance, biomass = [float(x) for x in line.split(',')[0:2]]
+            distance_biomass_lookup[distance].append(biomass)
+            count += 1
+            current_time = time.time()
+            if current_time - start_time > 3.0:
+                print '%.2f%% complete' % (100.0 * count / float(num_lines))
+                start_time = current_time
+
+        biomass_summary_uri = os.path.join(
+            CSV_DIR, '%s_biomass_summary.csv' % prefix)
+        biomass_summary_file = open(biomass_summary_uri, 'w')
+        for distance in sorted(distance_biomass_lookup.keys()):
+            print 'processing distance %.2f' % distance
+            biomass_list = sorted(distance_biomass_lookup[distance])
+            biomass_summary_file.write('%f' % distance)
+            biomass_summary_file.write(
+                ',%f,%f' % (biomass_list[0], biomass_list[-1]))
+            biomass_length = len(biomass_list)
+            for p_value in [0.01, 0.05, 0.1, 0.5, 1.0]:
+                tail_percent = p_value/2.0
+                left_index = int(biomass_length * tail_percent)
+                right_index = (biomass_length - 1) - left_index
+                biomass_summary_file.write(',%f,%f' %
+                    (biomass_list[left_index], biomass_list[right_index]))
+            biomass_summary_file.write(
+                ',%f\n' % (sum(biomass_list)/float(len(biomass_list))))
+
+
+
+
+
