26aebd178bb26e40197188b63752fa2f812745f9
Richard Sharp <richpsharp@gmail.com>
2014-07-07 14:43:44 -0700

    Opening the simulation
    RUnning through all 3 landmasses.

--- global_carbon_edge_effect.py
+++ global_carbon_edge_effect.py
@@ -17,10 +17,9 @@ FOREST_LULCS = [1, 2, 3, 4, 5]
 
 if __name__ == '__main__':
     #mask out forest LULCs
-
     raster_utils.create_directories([OUTPUT_DIR])
     
-    for prefix in ['am']:#, 'af', 'as']:
+    for prefix in ['am', 'af', 'as']:
         lulc_raw_uri = os.path.join(DATA_DIR, '%s%s' % (prefix, LULC_BASE))
         biomass_raw_uri = os.path.join(DATA_DIR, '%s%s' % (prefix, BIOMASS_BASE))
         
@@ -29,11 +28,11 @@ if __name__ == '__main__':
         lulc_uri = os.path.join(OUTPUT_DIR, "%s_lulc_aligned.tif" % (prefix))
         biomass_uri = os.path.join(OUTPUT_DIR, "%s_biomass_aligned.tif" % (prefix))
         
-        '''raster_utils.align_dataset_list(
+        raster_utils.align_dataset_list(
             [lulc_raw_uri, biomass_raw_uri], [lulc_uri, biomass_uri], ['nearest']*2,
             cell_size, 'intersection', 0, dataset_to_bound_index=None,
             aoi_uri=None, assert_datasets_projected=True, process_pool=None)
-        '''
+        
         lulc_nodata = raster_utils.get_nodata_from_uri(lulc_uri)
         biomass_nodata = raster_utils.get_nodata_from_uri(biomass_uri)
         
@@ -49,16 +48,16 @@ if __name__ == '__main__':
                 mask[lulc == lulc_code] = 0
             mask[lulc == lulc_nodata] = mask_nodata
             return mask
-            
-        '''raster_utils.vectorize_datasets(
+        
+        raster_utils.vectorize_datasets(
             [lulc_uri,], mask_nonforest, mask_uri, gdal.GDT_Byte,
             mask_nodata, cell_size, 'intersection', dataset_to_align_index=0,
             dataset_to_bound_index=None, aoi_uri=None,
             assert_datasets_projected=True, process_pool=None, vectorize_op=False,
             datasets_are_pre_aligned=True)
-        '''
+        
         forest_edge_distance_uri = os.path.join(OUTPUT_DIR, "%s_forest_edge.tif" % prefix)
-        #raster_utils.distance_transform_edt(mask_uri, forest_edge_distance_uri)
+        raster_utils.distance_transform_edt(mask_uri, forest_edge_distance_uri)
 
         forest_edge_nodata = raster_utils.get_nodata_from_uri(forest_edge_distance_uri)
         biomass_stats_uri = os.path.join(OUTPUT_DIR,  "%s_biomass_stats.csv" % prefix)
