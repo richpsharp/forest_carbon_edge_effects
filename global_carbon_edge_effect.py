2f9608585291494a90811887d08a0d031b9bab51
Richard Sharp <richpsharp@gmail.com>
2014-07-07 11:39:56 -0700

    generalizing data and output directories
    settting this up so it can be process pooled.

--- global_carbon_edge_effect.py
+++ global_carbon_edge_effect.py
@@ -6,8 +6,8 @@ import numpy
 from invest_natcap import raster_utils
 
 BIOMASS_PREFIXES = ['am', 'af', 'as']
-DATA_DIR = "C:\\Users\\rich\\Desktop"
-OUTPUT_DIR = "C:\\Users\\rich\\Desktop\\forest_edge_output"
+DATA_DIR = os.path.join("C:\\", "Users", "rich", "Desktop")
+OUTPUT_DIR = os.path.join("C:\\", "Users", "rich", "Desktop", "forest_edge_output")
 BIOMASS_BASE = '_biov2ct1.tif'
 LULC_BASE = '.tif'
 FOREST_LULCS = [1, 2, 3, 4, 5]
@@ -18,19 +18,19 @@ if __name__ == '__main__':
     raster_utils.create_directories([OUTPUT_DIR])
     
     for prefix in ['am', 'af', 'as']:
-        lulc_raw_uri = os.path.join(DATA_DIR, '%s%s"' % (prefix, LULC_BASE))
-        biomass_raw_uri = os.path.join(DATA_DIR, '%s%s"' % (prefix, BIOMASS_BASE))
+        lulc_raw_uri = os.path.join(DATA_DIR, '%s%s' % (prefix, LULC_BASE))
+        biomass_raw_uri = os.path.join(DATA_DIR, '%s%s' % (prefix, BIOMASS_BASE))
         
         cell_size = raster_utils.get_cell_size_from_uri(lulc_raw_uri)
         
         lulc_uri = os.path.join(OUTPUT_DIR, "%s_lulc_aligned.tif" % (prefix))
         biomass_uri = os.path.join(OUTPUT_DIR, "%s_biomass_aligned.tif" % (prefix))
         
-        raster_utils.align_dataset_list(
+        '''raster_utils.align_dataset_list(
             [lulc_raw_uri, biomass_raw_uri], [lulc_uri, biomass_uri], ['nearest']*2,
             cell_size, 'intersection', 0, dataset_to_bound_index=None,
             aoi_uri=None, assert_datasets_projected=True, process_pool=None)
-            
+           ''' 
         lulc_nodata = raster_utils.get_nodata_from_uri(lulc_uri)
         biomass_nodata = raster_utils.get_nodata_from_uri(biomass_uri)
         
@@ -47,13 +47,13 @@ if __name__ == '__main__':
             mask[lulc == lulc_nodata] = mask_nodata
             return mask
             
-        raster_utils.vectorize_datasets(
+        '''raster_utils.vectorize_datasets(
             [lulc_uri,], mask_nonforest, mask_uri, gdal.GDT_Byte,
             mask_nodata, cell_size, 'intersection', dataset_to_align_index=0,
             dataset_to_bound_index=None, aoi_uri=None,
             assert_datasets_projected=True, process_pool=None, vectorize_op=False,
             datasets_are_pre_aligned=True)
-        
+        '''
         forest_edge_distance_uri = os.path.join(OUTPUT_DIR, "%s_forest_edge.tif" % prefix)
         raster_utils.distance_transform_edt(mask_uri, forest_edge_distance_uri)
 
@@ -61,7 +61,7 @@ if __name__ == '__main__':
         biomass_stats_uri = os.path.join(OUTPUT_DIR,  "%s_biomass_stats.csv" % prefix)
         outfile = open(biomass_stats_uri, 'w')
         
-        biomass_ds = gdal.Open(biomass_uri, gdal.GA_ReadOnly))
+        biomass_ds = gdal.Open(biomass_uri, gdal.GA_ReadOnly)
         biomass_band = biomass_ds.GetRasterBand(1)
         
         forest_edge_distance_ds = gdal.Open(forest_edge_distance_uri)
