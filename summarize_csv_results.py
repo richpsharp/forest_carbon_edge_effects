ae11f999627638110323f1f7d308db4ad1140c47
Richard Sharp <richpsharp@gmail.com>
2014-07-07 20:20:42 -0700

    add histogram
    writing out the total number of points with that distance value

--- summarize_csv_results.py
+++ summarize_csv_results.py
@@ -32,6 +32,8 @@ if __name__ == '__main__':
         biomass_summary_uri = os.path.join(
             CSV_DIR, '%s_biomass_summary.csv' % prefix)
         biomass_summary_file = open(biomass_summary_uri, 'w')
+        
+        #iterate over increasing distance
         for distance in sorted(distance_biomass_lookup.keys()):
             print 'processing distance %.2f' % distance
             biomass_list = sorted(distance_biomass_lookup[distance])
@@ -39,16 +41,20 @@ if __name__ == '__main__':
             biomass_summary_file.write(
                 ',%f,%f' % (biomass_list[0], biomass_list[-1]))
             biomass_length = len(biomass_list)
+            
+            #print out a tuple, the lower and upper bound of whatever p_value
             for p_value in [0.01, 0.05, 0.1, 0.5, 1.0]:
                 tail_percent = p_value/2.0
                 left_index = int(biomass_length * tail_percent)
                 right_index = (biomass_length - 1) - left_index
                 biomass_summary_file.write(',%f,%f' %
                     (biomass_list[left_index], biomass_list[right_index]))
+            #print the average biomass value
             biomass_summary_file.write(
-                ',%f\n' % (sum(biomass_list)/float(len(biomass_list))))
-
+                ',%f' % (sum(biomass_list)/float(len(biomass_list))))
+                
+            #print the count
+            biomass_summary_file.write(',%d\n' % biomass_length)
+            
     raster_utils.email_report(
         "done with summarize_csv_results.py", "3152624786@txt.att.net")
-
-
