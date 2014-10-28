a87ecb28a64cb95d21a0b2a47e05037be930dadc
Richard Sharp <richpsharp@gmail.com>
2014-10-27 21:03:09 -0700

    fixing an issue where the csv file changed and the csv header changed

--- annotate_shapefiles.py
+++ annotate_shapefiles.py
@@ -1,3 +1,4 @@
+from types import StringType
 import os
 
 import ogr
@@ -6,21 +7,21 @@ from invest_natcap import raster_utils
 
 
 def annotate_ecoregions(ecoregion_uri, table_uri):
+    lookup_table = raster_utils.get_lookup_from_csv(table_uri, 'region')
+    
     print 'generating report'
     esri_driver = ogr.GetDriverByName('ESRI Shapefile')
 
     original_datasource = ogr.Open(ecoregion_uri)
-    updated_datasource_uri = os.path.join(os.path.dirname(ecoregion_uri), 'annotated_ecoregions.shp')
+    updated_datasource_uri = os.path.join(os.path.dirname(ecoregion_uri), 'annotated_ecoregions_2.shp')
     #If there is already an existing shapefile with the same name and path, delete it
-    #Copy the input shapefile into the designated output folder
+    #Copy the input shapefile into the designated output floder
     if os.path.isfile(updated_datasource_uri):
         os.remove(updated_datasource_uri)
     datasource_copy = esri_driver.CopyDataSource(original_datasource, updated_datasource_uri)
     layer = datasource_copy.GetLayer()
 
-    new_field_names = [('(core-edge)/core', 'magnitude'), ('A(80)', 'A80'), ('A(90)', 'A90'), ('A(95)', 'A95')]
-
-    lookup_table = raster_utils.get_lookup_from_csv(table_uri, 'region')
+    new_field_names = [('Magnitude', 'magnitude'), ('A(80)', 'A80'), ('A(90)', 'A90'), ('A(95)', 'A95')]
 
 
     for table_field, field_name in new_field_names:
@@ -30,14 +31,21 @@ def annotate_ecoregions(ecoregion_uri, table_uri):
     for feature_id in xrange(layer.GetFeatureCount()):
         feature = layer.GetFeature(feature_id)
 
-        feature_eco_name = feature.GetField('ECO_NAME')
+        feature_eco_name = raster_utils._smart_cast(feature.GetField('ECO_NAME'))
+        
         for table_field, field_name in new_field_names:
             try:
                 value = lookup_table[feature_eco_name][table_field]
+                if feature_eco_name == 'Balsas Dry Forests':
+                    print feature_eco_name, table_field, value
                 feature.SetField(field_name, float(value))
-            except KeyError:
+            except KeyError as e:
+                if feature_eco_name == 'Balsas Dry Forests':
+                    print e
                 feature.SetField(field_name, -1.0)
             except TypeError:
+                if feature_eco_name == 'Balsas Dry Forests':
+                    print e
                 feature.SetField(field_name, -1.0)
 
         #Save back to datasource
@@ -46,5 +54,5 @@ def annotate_ecoregions(ecoregion_uri, table_uri):
 
 if __name__ == '__main__':
     ecoregion_uri = "C:/Users/rich/Desktop/ecoregions/ecoregions_projected.shp"
-    table_uri = "C:/Users/rich/Documents/Dropbox/Edge Carbon Effects/Spreadsheets/edge_effects_forest_ecoregions.csv"
+    table_uri = "C:/Users/rich/Desktop/edge_effects_ecoregions.csv"
     annotate_ecoregions(ecoregion_uri, table_uri)
